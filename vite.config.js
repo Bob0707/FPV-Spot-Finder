bcd939ad117db64994cf49f2d6427034067a0cd3
Bob0707 <bochimenko@gmail.com>
2026-04-12 15:34:11 +0200

    #

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FPV-Spot-Finder/',
   server: {
     proxy: {
       // ── Overpass-API Endpoints (Phase 4) ───────────────────────────────
